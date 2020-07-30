9760e4c381a27a90248f626a7bd9d3ad4af661e6
Jason Merino <jason.merino@ferguson.com>
2020-07-30 07:29:20 -0700

    Update main app view to show snippets.

--- SnipBoard/ContentView.swift
+++ SnipBoard/ContentView.swift
@@ -7,12 +7,62 @@
 //
 
 import SwiftUI
+import CoreData
+
+struct SnippetItem: Identifiable {
+    var id = UUID()
+    var createdAt = Date()
+    var text: String
+}
 
 struct ContentView: View {
-    @State private var name = ""
+    @State private var newSnippetItem = ""
+    @State private var showingAlert = false
+    @State private var snippetItems = [SnippetItem]()
+    
+    private func getDate(date: Date) -> String {
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = "MMM dd, YYYY"
+        return dateformat.string(from: date)
+    }
     
     var body: some View {
-        TextField("Enter text", text: $name)
+        NavigationView {
+            List {
+                Section(header: Text("Add a new snippet")) {
+                    HStack {
+                        TextField("New snippet", text: $newSnippetItem)
+                        Button(action: {
+                            
+                            if (self.newSnippetItem == "") {
+                                self.showingAlert = true
+                                return
+                            }
+                            
+                            self.snippetItems.append(SnippetItem(text: self.newSnippetItem))
+                            
+                            self.newSnippetItem = ""
+                        }) {
+                            Image(systemName: "plus.circle.fill").foregroundColor(.orange)
+                                .imageScale(.large)
+                        }.alert(isPresented: $showingAlert) {
+                            Alert(title: Text("Nice try..."), message: Text("Enter a snippet before trying to save."), dismissButton: .default(Text("Got it!")))
+                        }
+                    }
+                }.font(.headline)
+                Section(header: Text("Snippets")) {
+                    ForEach(self.snippetItems) {snippetItem in
+                        VStack(alignment: .leading) {
+                            Text(snippetItem.text).font(.headline)
+                            Text(self.getDate(date: snippetItem.createdAt)).font(.caption)
+                        }
+                    }.onDelete { indexSet in
+                        print("delete")
+                    }
+                }
+            }
+            .navigationBarTitle(Text("SnipBoard"))
+        }
     }
 }
 
